2a36ddf13eaacb0cd11fcf44e0e38f8c0403bc4f
Katharine Hedlund <Klh875@gmail.com>
2022-05-29 18:08:53 -0500

    Update database schema! Fix get queries to return JSON

--- database/create_database.py
+++ database/create_database.py
@@ -16,50 +16,37 @@ c.execute('''
 # courses table
 c.execute('''
           CREATE TABLE courses (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              course_id INTEGER PRIMARY KEY AUTOINCREMENT,
               course_name character varying(50),
               instructor_username character,
-              instructor_name character,
-              FOREIGN KEY (instructor_username) REFERENCES users(username),
-              FOREIGN KEY (instructor_name) REFERENCES users(name)
+              FOREIGN KEY (instructor_username) REFERENCES users(username)
           )
           ''')
 
-# teaches course table
-# links courses to people enrolled in or teaching that course
-c.execute('''
-          CREATE TABLE takes_course (
-                username character,
-                course_id INTEGER, 
-                FOREIGN KEY (course_id) REFERENCES courses(id),
-                FOREIGN KEY (username) REFERENCES users(username),
-                PRIMARY KEY (course_id, username)
-          )
-          ''')
-
-# takes course table
-c.execute('''
-          CREATE TABLE teaches_course (
-                username character,
-                course_id INTEGER, 
-                FOREIGN KEY (course_id) REFERENCES courses(id),
-                FOREIGN KEY (username) REFERENCES users(username),
-                PRIMARY KEY (course_id)
-          )
-          ''')
 
 # announcements table
 c.execute('''
           CREATE TABLE announcements (
-                id INTEGER PRIMARY KEY AUTOINCREMENT ,
+                announcement_id INTEGER PRIMARY KEY AUTOINCREMENT ,
                 course_id INTEGER,
                 title character NOT NULL,
-                author character,
                 content TEXT NOT NULL,
-                FOREIGN KEY (course_id) REFERENCES courses(id),
-                FOREIGN KEY (author) REFERENCES users(name)
+                FOREIGN KEY (course_id) REFERENCES courses(course_id)
           )
           ''')
+
+# take course table
+c.execute('''
+          CREATE TABLE takes_course (
+                username character, 
+                course_id INTEGER, 
+                FOREIGN KEY (course_id) REFERENCES courses(course_id), 
+                FOREIGN KEY (username) REFERENCES users(username),
+                PRIMARY KEY (username, course_id)
+          )
+          ''')
+
+
 
 
 # assignments table
@@ -75,20 +62,22 @@ c.executescript('''
           INSERT INTO users VALUES('patrick_whalen', 'student', '12345', 'Patrick Whalen');
           INSERT INTO users VALUES('gerry1954', 'instructor', '12345', 'Gerry Brady');
           INSERT INTO users VALUES('chelsea_troy', 'instructor', '12345', 'Chelsea Troy');
-          INSERT INTO users VALUES('t_terry', 'instructor', '12345', 'Yosvany Terry');
+          INSERT INTO users VALUES('y_terry', 'instructor', '12345', 'Yosvany Terry');
+          INSERT INTO users VALUES('rafi_a', 'instructor', '12345', 'Rafi Almhana');
+          INSERT INTO users VALUES('z_shang', 'instructor', '12345', 'Zechao Shang');
           ''')
 
 
 # add courses to db
 conn.executescript('''
-        INSERT INTO courses VALUES(null, 'Intro to Mobile Dev', 'Chelsea Troy', 'chelsea_troy');
-         INSERT INTO courses VALUES(null, 'Algorithms', 'Gerry Brady', 'gerry1954');
-         INSERT INTO courses VALUES(null, 'Web Dev', 'Rafi Almhana', 'rafi_a');
-         INSERT INTO courses VALUES(null, 'Advanced Algorithms', 'Gerry Brady', 'gerry1954');
-         INSERT INTO courses VALUES(null, 'Intro to Python Programming', 'Chelsea Troy', 'chelsea_troy');
-         INSERT INTO courses VALUES(null, 'Databases', 'Zechao Shang', 'z_shang');
-         INSERT INTO courses VALUES(null, 'Discrete Mathematics', 'Gerry Brady', 'gerry1954');
-         INSERT INTO courses VALUES(null, 'Afro Cuban Rhythms', 'Yosvany Terry', 'y_terry');
+        INSERT INTO courses VALUES(null, 'Intro to Mobile Dev', 'chelsea_troy');
+         INSERT INTO courses VALUES(null, 'Algorithms', 'gerry1954');
+         INSERT INTO courses VALUES(null, 'Web Dev', 'rafi_a');
+         INSERT INTO courses VALUES(null, 'Advanced Algorithms', 'gerry1954');
+         INSERT INTO courses VALUES(null, 'Intro to Python Programming', 'chelsea_troy');
+         INSERT INTO courses VALUES(null, 'Databases', 'z_shang');
+         INSERT INTO courses VALUES(null, 'Discrete Mathematics', 'gerry1954');
+         INSERT INTO courses VALUES(null, 'Afro Cuban Rhythms', 'y_terry');
 ''')
 
 # associate students with courses
@@ -115,21 +104,8 @@ c.executescript('''
           INSERT INTO takes_course VALUES('ming_liu', 7);
           INSERT INTO takes_course VALUES('patrick_whalen', 7);
           INSERT INTO takes_course VALUES('klh875', 8);
-          INSERT INTO takes_course VALUES('ming_liu', 8);
-          INSERT INTO takes_course VALUES('patrick_whalen', 8);
           ''')
 
-# associate instructors with courses
-c.executescript('''
-          INSERT INTO teaches_course VALUES('chelsea_troy', 1);
-          INSERT INTO teaches_course VALUES('gerry1954', 2);
-          INSERT INTO teaches_course VALUES('rafi_a', 3);
-          INSERT INTO teaches_course VALUES('gerry1954', 4);
-          INSERT INTO teaches_course VALUES('chelsea_troy', 5);
-          INSERT INTO teaches_course VALUES('z_shang', 6);
-          INSERT INTO teaches_course VALUES('gerry1954', 7);
-          INSERT INTO teaches_course VALUES('y_terry', 8);
-          ''')
 
 # add data to announcements
 
